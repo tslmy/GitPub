631fc0359b9c650034e4b5bacc076846bb3cc1bf
Mingyang Li <tslmy@outlook.com>
2016-07-02 16:20:35 -0400

    Rewrite index.html with no jquery library

--- index.js
+++ index.js
@@ -1,54 +1,69 @@
-$(function (){
-   var k33g = new Gh3.User("tslmy")
-      ,	repoTitle = $("h1")
-      ,	branchTitle = $("h2")
-      ,	branchContents = $("ul");
+(function($) {
+  var $List;
+  $List = $('#list');
+  /* Polyfill for IE<9 for handling negative a index in `substr`
+   * 
+   * Only run when the substr function is broken
+   */
+  if ('ab'.substr(-1) != 'b') {
+    /**
+     *  Get the substring of a string
+     *  @param  {integer}  start   where to start the substring
+     *  @param  {integer}  length  how many characters to return
+     *
+     *  @return {string}
+     */
+    String.prototype.substr = function(substr) {
+      return function(start, length) {
+        // did we get a negative start, calculate how much it is
+        // from the beginning of the string
+        if (start < 0) start = this.length + start;
+        // call the original function
+        return substr.call(this, start, length);
+      }
+    }(String.prototype.substr);
+  }
 
-    var k33gBlog = new Gh3.Repository("k33g.github.com", k33g);
+  function addFilesToList(p_sPath, p_$List, p_sRootUrl, p_sUser, p_sRepo, p_sToken) {
+    var sUrl = 'https://api.github.com/repos/' + p_sUser + '/' + p_sRepo + '/contents/' + p_sPath + (p_sToken ? '?access_token=' + p_sToken : '');
+    $.ajax({
+      dataType: "json",
+      url: sUrl,
+      success: function(p_oData) {
+        $.each(p_oData, function(p_i, p_oFile) {
+          var $SubList, sFileType, sExtension;
 
-    k33gBlog.fetch(function (err, res) {
-      if(err) { throw "outch ..." }
+          if (typeof p_oFile === 'undefined' || typeof p_oFile.name === 'undefined') {
+            return false;
+          }
 
-      repoTitle.html(k33gBlog.full_name);
-
-      k33gBlog.fetchBranches(function (err, res) {
-        if(err) { throw "outch ..." }
-
-        var master = k33gBlog.getBranchByName("master");
-        branchTitle.html(master.name + " (" + master.sha + ") :");
-
-        master.fetchContents(function (err, res) {
-          if(err) { throw "outch ..." }
-
-          master.eachContent(function (content) {
-            branchContents.append('<li>'+content.name+" : "+content.type);
-
-          });
-
-          //var myfile = master.getContents()[8];
-
-          var myfile = master.getFileByName("index.html");
-
-          console.log("index.html", myfile);
-
-          myfile.fetchContent(function (err, res) {
-            if(err) { throw "outch ..." }
-
-            rawContent.text(myfile.getRawContent());
-          });
-
-          myfile.fetchCommits(function (err, res) {
-            if(err) { throw "outch ..." }
-
-            console.log(myfile.getCommits());
-
-            myfile.eachCommit(function (commit) {
-              console.log(commit.author.login, commit.message, commit.date);
-            });
-          });
-
+          if (p_oFile.size === null || p_oFile.size === 0) {
+            // Directory
+            $SubList = $('<ul></ul>');
+            addFilesToList(p_oFile.path, $SubList, p_sRootUrl, p_sUser, p_sRepo, p_sToken);
+            p_$List.append(
+              '<li class="entry folder">' + ' <a href="' + p_sRootUrl + p_sPath + '/' + p_oFile.name + '/">' + p_oFile.name + '</a></li>'
+            ).append($SubList);
+          } else { // File
+            sFileType = 'file';
+            sExtension = p_oFile.name.substr(-3);
+            sFileName = p_oFile.name.substr(0, p_oFile.name.length - 3)
+            if (sExtension == '.md' && p_oFile.name.substr(0, 1) != '.')
+              p_$List.append(
+                '<li class="entry file">' + ' <a href="' + p_sRootUrl + p_sPath + '/' + p_oFile.name + '">' + sFileName + '</a></li>'
+              );
+          }
         });
-
-      })
+      },
+      error: function(p_oJqXHR, p_sStatus, p_sError) {
+        p_$List.append('<li class="entry error">' + p_sStatus + ': ' + p_sError + '</li>');
+      }
     });
-  });
+  }
+  var sUser = "tslmy",
+    sRepo = "GitPub",
+    sToken = '',
+    sRootUrl = 'http://github.com/' + sUser + '/' + sRepo + '/';
+  addFilesToList('', $List, sRootUrl, sUser, sRepo, sToken);
+}(jQuery));
+/*EOF*/

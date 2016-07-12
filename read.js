a233fe9b6981d548246db9be39bab24e736ea718
Mingyang Li <tslmy@outlook.com>
2016-07-12 17:34:49 -0400

    disqus

--- read.js
+++ read.js
@@ -1,15 +1,22 @@
 var domainArray = window.location.host.split('.');
 var pathArray = window.location.pathname.split('/');
-if (domainArray.length == 3 && domainArray[1].toLowerCase() == 'github' && domainArray[2].toLowerCase() == 'io') {
-    sUser = domainArray[0];
-    sRepo = pathArray[1];
-} else {
-    sUser = 'tslmy';
-    sRepo = 'GitPub';
-};
-var sBaseUrl = 'https://raw.githubusercontent.com/' + sUser + '/' + sRepo + '/master/';
-    var sFilePath = window.location.search.replace("?", "");
-    $(document).ready( function() { 
-     $('title').text(decodeURI(sFilePath.substr(0,sFilePath.length-3)));
-     $("textarea").load(sBaseUrl + sFilePath, function() { $('body').append('<script src="strapdown.js"></script>');
-    })});
+(function() { 
+	if (domainArray.length == 3 && domainArray[1].toLowerCase() == 'github' && domainArray[2].toLowerCase() == 'io') {
+		sUser = domainArray[0];
+		sRepo = pathArray[1];
+	} else {
+		sUser = 'tslmy';
+		sRepo = 'GitPub';
+	};
+	var sBaseUrl = 'https://raw.githubusercontent.com/' + sUser + '/' + sRepo + '/master/';
+		var sFilePath = window.location.search.replace("?", "");
+		$(document).ready( function() { 
+		 $('title').text(decodeURI(sFilePath.substr(0,sFilePath.length-3)));
+		 $("textarea").load(sBaseUrl + sFilePath, function() { $('body').append('<script src="strapdown.js"></script>');
+		})});
+	// Disqus
+	var d = document, s = d.createElement('script');
+	s.src = '//gitpub.disqus.com/embed.js';
+	s.setAttribute('data-timestamp', +new Date());
+	(d.head || d.body).appendChild(s);
+})();

61c7712c68338bc453a42b3cc29ea0d77ee11d08
Mingyang Li <tslmy@outlook.com>
2016-07-28 19:29:01 -0400

    Background check

--- read.js
+++ read.js
@@ -1,5 +1,43 @@
 var domainArray = window.location.host.split('.');
 var pathArray = window.location.pathname.split('/');
+
+//https://jsfiddle.net/s7Wx2/
+ function getImageBrightness(imageSrc,callback) {
+        var img = document.createElement("img");
+        img.src = imageSrc;
+        img.style.display = "none";
+        document.body.appendChild(img);
+
+        var colorSum = 0;
+
+        img.onload = function() {
+            // create canvas
+            var canvas = document.createElement("canvas");
+            canvas.width = this.width;
+            canvas.height = this.height;
+
+            var ctx = canvas.getContext("2d");
+            ctx.drawImage(this,0,0);
+
+            var imageData = ctx.getImageData(0,0,canvas.width,canvas.height);
+            var data = imageData.data;
+            var r,g,b,avg;
+
+              for(var x = 0, len = data.length; x < len; x+=4) {
+                r = data[x];
+                g = data[x+1];
+                b = data[x+2];
+
+                avg = Math.floor((r+g+b)/3);
+                colorSum += avg;
+            }
+
+            var brightness = Math.floor(colorSum / (this.width*this.height));
+            callback(brightness);
+        }
+    }
+
+
 (function() { 
 	if (domainArray.length == 3 && domainArray[1].toLowerCase() == 'github' && domainArray[2].toLowerCase() == 'io') {
 		sUser = domainArray[0];
@@ -19,18 +57,28 @@ var pathArray = window.location.pathname.split('/');
 	  smartypants: true
 	});
 	var sBaseUrl = 'https://raw.githubusercontent.com/' + sUser + '/' + sRepo + '/master/';
-		var sFilePath = window.location.search.replace("?", "");
-		var sTitle = sFilePath.substr(0, sFilePath.lastIndexOf("."));
-		var sImagePath = sTitle + ".jpg";
-		$(document).ready( function() { 
-		 $('title').text(decodeURI(sTitle));
-		 $('article').load(sBaseUrl + sFilePath, function(content) {
-		 	marked(content, function (err, content) {
-			  if (err) throw err;
-			  $('article').html(content);
-			});
-		 	//$('body').append('<script src="strapdown.js"></script>');
-		})});
+	var sFilePath = window.location.search.replace("?", "");
+	var sTitle = sFilePath.substr(0, sFilePath.lastIndexOf("."));
+	$('title').text(decodeURI(sTitle));
+	$('header > h1').text(decodeURI(sTitle));
+	$('article').load(sBaseUrl + sFilePath, function(content) {
+		marked(content, function (err, content) {
+			if (err) throw err;
+			$('article').html(content);
+		});
+	});
+	//load header
+	var sImagePath = sTitle + ".jpg";
+	var sImageUrl = sBaseUrl + sImagePath;
+	$("header").css("background-image", "url("+sImageUrl+")");
+	var sAvatarUrl = "https://avatars.githubusercontent.com/" + sUser;
+	$("header > img").prop("src", sAvatarUrl);
+	$("header > a").text(sUser);
+	$("header > a").prop("href", "https://github.com/"+sUser);
+	getImageBrightness(sImageUrl, function(brightness) {
+		var color = 255 - brightness;
+		$('header > a, header > h1').css('background','rgb('+color+', '+color+', '+color+')');
+	});
 	// Disqus
 	var d = document, s = d.createElement('script');
 	s.src = '//gitpub.disqus.com/embed.js'; //USERS OF GITPUB: CHANGE THIS "GITPUB"!
